78802451e60689124b3e88c748f90e4bb83886c5
Senorita2013 <iamsenoritamishra@gmail.com>
2025-08-27 18:31:26 +0100

    Add files via upload

---
+++ trignometry.py
@@ -0,0 +1,12 @@
+import math
+
+x = 45
+angle_radians = math.radians(x)
+
+sin_value = math.sin(angle_radians)
+cos_value = math.cos(angle_radians)
+tan_value = math.tan(angle_radians)
+
+print("sin =", sin_value)
+print("cos =", cos_value)
+print("tan =", tan_value)
